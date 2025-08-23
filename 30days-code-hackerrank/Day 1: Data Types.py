545f4861042c959685296148a994632d9e78a960
Komal Harshita <komal.sony234@gmail.com>
2025-08-23 15:56:10 +0530

    Create Day 1: Data Types.py

---
+++ 30days-code-hackerrank/Day 1: Data Types.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j = int(input())
+fl = float(input())
+st = str(input())
+# Read and save an integer, double, and String to your variables.
+i += j
+d += fl
+s += st
+# Print the sum of both integer variables on a new line.
+print(i)
+print(d)
+print(s)
