f066445686eeafc3672582382c46badbc0052ad5
Komal Harshita <komal.sony234@gmail.com>
2025-08-28 19:54:10 +0530

    Create Day 18: Queues and Stacks.py

---
+++ 30days-code-hackerrank/Day 18: Queues and Stacks.py
@@ -0,0 +1,37 @@
+import sys
+
+class Solution:
+    def __init__(self):
+        self.stack=[]
+        self.queue=[]
+    def pushCharacter(self,char):
+        self.stack.append(char)
+        return self.stack
+    def popCharacter(self):
+        return self.stack.pop()
+    def enqueueCharacter(self,char):
+        self.queue.append(char)
+        return self.queue
+    def dequeueCharacter(self):
+        return self.stack.pop(0)
+
+s=input()
+obj=Solution()   
+
+l=len(s)
+
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+    
+isPalindrome=True
+
+for i in range(l // 2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+
+if isPalindrome:
+    print("The word, "+s+", is a palindrome.")
+else:
+    print("The word, "+s+", is not a palindrome.")   
